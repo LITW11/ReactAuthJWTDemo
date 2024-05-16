99ec4240a8ef5b8e57a39b2dd1e6b3e2f9a325a7
Avrumi Friedman <a.friedman07@gmail.com>
2024-05-15 22:05:02 -0400

    Add project files.

---
+++ ReactAuthJWTDemo.Web/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ReactAuthJWTDemo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getdata")]
+        public object GetSecretData()
+        {
+            var currentUser = User.FindFirst(ClaimTypes.Email).Value; //this is instead of User.Identity.Name
+            return new { message = $"This is a huge secret!! and you are {currentUser}" };
+        }
+    }
+}
